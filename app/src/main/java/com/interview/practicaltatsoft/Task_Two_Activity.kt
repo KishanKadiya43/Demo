62a56d1167d22bd2d063162fece0fd5e7901347d
AKASH BHARDWAJ <akash@innovateinfinite.com>
2021-05-22 10:59:00 +0530

    test

---
+++ app/src/main/java/com/interview/practicaltatsoft/Task_Two_Activity.kt
@@ -0,0 +1,11 @@
+package com.interview.practicaltatsoft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Task_Two_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task__two_)
+    }
+}
